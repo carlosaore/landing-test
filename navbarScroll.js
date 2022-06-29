e1ac237496dfd9f7791b69b6c60cafa18a35900e
Carlos Orellana <59698756+carlosaore@users.noreply.github.com>
2022-06-29 18:02:55 +0200

    Added hero image and accordion

--- navbarScroll.js
+++ navbarScroll.js
@@ -4,7 +4,7 @@ window.onscroll = function () {
 	if (prevScrollpos > currentScrollPos) {
 		document.getElementById("site-header__header").style.top = "0";
 	} else {
-		document.getElementById("site-header__header").style.top = "-100px";
+		document.getElementById("site-header__header").style.top = "-180px";
 	}
 	prevScrollpos = currentScrollPos;
 }
