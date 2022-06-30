da23ef165a3c079e06849bb4a0e044080e790483
Carlos Orellana <59698756+carlosaore@users.noreply.github.com>
2022-06-30 11:07:00 +0200

    fixed issue with scroll rebound in iphone

--- navbarScroll.js
+++ navbarScroll.js
@@ -1,7 +1,7 @@
 let prevScrollpos = window.scrollY;
 window.onscroll = function () {
 	let currentScrollPos = window.scrollY;
-	if (prevScrollpos > currentScrollPos) {
+	if (prevScrollpos > currentScrollPos || currentScrollPos <= 120) {
 		document.getElementById("site-header__header").style.top = "0";
 	} else {
 		document.getElementById("site-header__header").style.top = "-180px";
